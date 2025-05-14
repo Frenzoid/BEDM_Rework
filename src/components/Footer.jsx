290f2082720592446299657569ba4132a5d05b7a
Frenzoid <frenzoid@protonmail.com>
2025-05-14 19:06:23 +0200

    fixed year on footer and leaderboard

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ import { useTranslation } from 'react-i18next';
 import '../styles/Footer.css';
 
 const Footer = () => {
-  const { t, i18n } = useTranslation();
+  const { t } = useTranslation();
   const currentYear = new Date().getFullYear();
 
   return (
